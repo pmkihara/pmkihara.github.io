486af2381a0cd4c323c46ca5e972bf2e13af94bb
Patricia Kihara <patricia.kihara@outlook.com>
2022-03-05 21:35:21 -0300

    Page slide animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const tabs = document.querySelectorAll('.tab')
+
+const changeTab = (event) => {
+  const targetTab = event.currentTarget;
+  const targetId = targetTab.id.replace(/tab-/, '');
+  const targetContent = document.getElementById(`content-${targetId}`);
+  const oldActiveTab = document.querySelector('.tab.active');
+  const oldActiveContent = document.querySelector('.content.active');
+
+  // PREVIOUS TAB/CONTENT
+  // Remove the active class from the tab and the content
+  oldActiveTab.classList.remove('active');
+  oldActiveContent.classList.remove('active');
+  // Change the icon
+  oldActiveTab.querySelector('i').classList.remove('fa-arrow-circle-up');
+  oldActiveTab.querySelector('i').classList.add('fa-plus-circle');
+
+  // SELECTED TAB/CONTENT
+  // Add the active class from the tab and the content
+  targetTab.classList.add('active');
+  targetContent.classList.add('active');
+  // Change the icon
+  targetTab.querySelector('i').classList.remove('fa-plus-circle');
+  targetTab.querySelector('i').classList.add('fa-arrow-circle-up');
+}
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', changeTab);
+})
--- index.md
+++
@@ -1,8 +0,0 @@
----
-layout: default
-title: "Patricia Mey Kihara"
----
-
-<h1>Hello!</h1>
-<h2>I'm Patricia</h2>
-<p>Full-Stack Web Developer</p>
